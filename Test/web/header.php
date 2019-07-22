fa5859d9ce42de4221187ba4b4ab065375f5d06c
Jan-Nguyen38 <thaouit10@gmail.com>
2019-07-22 23:44:00 +0700

    Create header.php

---
+++ Test/web/header.php
@@ -0,0 +1,8 @@
+<h5>
+	<a href="home.php">Trang chủ </a>|
+	<a href="add_student_view.php"> Thêm sinh viên </a>|
+	<a href="add_subject_view.php"> Thêm môn học</a> |
+	<a href="login.php?status=logout"> Đăng xuất </a>
+
+</h5>
+
